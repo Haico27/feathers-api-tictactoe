9cc12cab08069cd1ea3ef13e457462c7986d1efa
Haico27 <haico.wensink@gmail.com>
2017-02-19 01:49:12 +0100

    added the first seeds to check whether it is working

---
+++ src/seeds.js
@@ -0,0 +1,57 @@
+const feathers = require('feathers-client');
+const authentication = require('feathers-authentication');
+const rest = require('feathers-rest/client');
+const superagent = require('superagent');
+const host = 'http://localhost:3030';
+const app = feathers()
+  .configure(feathers.hooks())
+  .configure(feathers.authentication({
+    type: 'local'
+  }))
+  .configure(rest(host).superagent(superagent));
+
+// services
+const userService = app.service('users');
+const gameService = app.service('games');
+
+const user = {
+  name: 'Roger Federer',
+  email: 'roger@federer.com',
+  password: 'abcd1234'
+}
+
+const games = [
+  {
+  title: 'Game 1'
+},
+{
+  title: 'Game 2'
+}]
+
+// Seed the user and game!
+userService.create(user)
+  .then((result) => {
+    console.log('User created, authenticating as user...');
+
+    app.authenticate({
+      type: 'local',
+      email: user.email,
+      password: user.password,
+    }).then((result) => {
+      console.log('Authenticated, seeding games...');
+
+      games.map((game) => {
+        gameService.create(Object.assign({}, game, { token: result.token }))
+          .then((result) => {
+            console.log('Game seeded...');
+          }).catch((error) => {
+            console.error('Error seeding game!', error);
+          });
+      })
+    }).catch((error) => {
+      console.error('Error authenticating!', error);
+    });
+  })
+  .catch((error) => {
+    console.error('Error creating user!', error);
+  });
